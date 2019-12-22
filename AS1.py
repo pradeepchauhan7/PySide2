670b74e0eff14f46d42a1b9e10c857dc0a184a50
pradeepchauhan <pradeep.chauhan79.pc@gmail.com>
2019-12-22 19:17:34 +0530

    Update AS1.py

--- AS1.py
+++ AS1.py
@@ -1,5 +1,6 @@
 from PySide2.QtCore import QRect, Slot, qApp, QEvent
-from PySide2.QtWidgets import QMainWindow, QApplication, QGraphicsScene, QGraphicsView, QPushButton, QWidget
+from PySide2.QtWidgets import (QMainWindow, QApplication, QGraphicsScene, QGraphicsView, QPushButton,
+                               QWidget, QLineEdit, QHBoxLayout, QGroupBox, QSizeGrip, QSplitter, QPlainTextEdit)
 from PySide2.QtCore import Qt, QPoint
 from PySide2.QtGui import QImage, QPainter, QPen, QPixmap
 from PySide2 import QtCore
@@ -13,47 +14,43 @@ class MainWindow(QMainWindow):
         self.scene = QGraphicsScene()
         self.resize(800, 600)
         self.button = QPushButton("Draw Text Boxes")
+
         self.scene.addWidget(self.button)
-        self.image = QImage(self.size(), QImage.Format_RGB32)
-        self.image.fill(Qt.black)
 
-
-
-        self.view = QGraphicsView(self.scene)
+        self.view = QGraphicsView()
+        self.scene.setBackgroundBrush(Qt.blue)
+        self.view.setScene(self.scene)
         self.setCentralWidget(self.view)
-
         self.button.clicked.connect(self.buttonClicked)
         self.view.viewport().installEventFilter(self)
 
         self.drawing = False
-        self.brushSize = 2
-        self.brushColor = Qt.white
         self.lastPoint = QPoint()
         self.startPoint = QPoint()
 
     def mousePressEvent(self, event):
-        #        super(MainWindow, self).mousePressEvent(event)
-        if event.button() == Qt.LeftButton:
+        if (event.button() == Qt.LeftButton):
             self.startPoint = event.pos()
             self.drawing = True
+
     def mouseReleaseEvent(self, event):
 
-        if (Qt.LeftButton & self.drawing):
-
+        if (Qt.LeftButton and self.drawing ):
             self.lastPoint = event.pos()
-            painter = QPainter(self.image)
-            painter.setPen(QPen(self.brushColor, self.brushSize, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
-            painter.drawRect(QtCore.QRect(self.startPoint,self.lastPoint))
-            print(QtCore.QRect(self.startPoint, self.lastPoint).size())
             self.update()
 
-
     def paintEvent(self, event):
-        if (self.drawing):
-            pixmap = QPixmap.fromImage(self.image)
-            self.scene.addPixmap(pixmap)
-        #    canvasPainter = QPainter(self)
-        #    canvasPainter.drawImage(self.rect(), self.image, self.image.rect())
+        if (self.drawing and self.button.click()):
+
+            le = QPlainTextEdit()
+            width = QtCore.QRect(self.startPoint, self.lastPoint).size().width()
+            height = QtCore.QRect(self.startPoint, self.lastPoint).size().height()
+            x = self.startPoint.x()
+            y = self.startPoint.y()
+            print(x)
+            if (width > 1 and height > 1):
+                le.setGeometry(x, y, width, height)
+                self.scene.addWidget(le)
 
     def buttonClicked(self):
         self.button.hide()
@@ -62,11 +59,8 @@ class MainWindow(QMainWindow):
         if obj is self.view.viewport():
             if event.type() == QEvent.MouseButtonPress:
                 self.mousePressEvent(event)
-                print('mouse press event = ', event.pos())
             elif event.type() == QEvent.MouseButtonRelease:
                 self.mouseReleaseEvent(event)
-                print('mouse release event = ', event.pos())
-
         return QWidget.eventFilter(self, obj, event)
 
 
@@ -74,6 +68,6 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
 
     window = MainWindow()
-    window.resize(800, 600)
+#    window.resize(800, 600)
     window.show()
     sys.exit(app.exec_())
